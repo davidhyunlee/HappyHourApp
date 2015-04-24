e7e65d80fb938e771b4d1cca3614c3bcb9f0545a
David Lee <davidhyunlee@gmail.com>
2015-04-24 10:46:26 -0700

    stylized business edit form. added hours controller

--- app/controllers/businesses_controller.rb
+++ app/controllers/businesses_controller.rb
@@ -1,4 +1,8 @@
 class BusinessesController < ApplicationController
+	def index
+		@businesses = Business.all
+	end
+
 	def new
 		@business = Business.new
 	end
--- app/models/business.rb
+++ app/models/business.rb
@@ -3,6 +3,7 @@ class Business < ActiveRecord::Base
   has_many :reviews
   has_many :users, through: :photos
   has_many :users, through: :reviews
+  has_one :hour
 
   def date_added
     created_at.localtime.strftime("%-m/%-d/%Y | %l:%M %p")
