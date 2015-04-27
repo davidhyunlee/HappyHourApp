bb786fca93e937b723ae0db89bc5722aa6728610
David Lee <david@davidlee.io>
2015-04-27 03:55:39 -0700

    modified scss file to remedy heroku bugs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,5 @@
 Rails.application.configure do
-  config.serve_static_assets = true
+  config.serve_static_files = true
   config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
   config.assets.compile = true
   # Settings specified here will take precedence over those in config/application.rb.
