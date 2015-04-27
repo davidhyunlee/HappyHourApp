a914a3f558cf3114eefd26253c8008b33341f0ae
David Lee <david@davidlee.io>
2015-04-27 03:49:36 -0700

    modified page index

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,6 @@
 Rails.application.configure do
   config.serve_static_assets = true
+  config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
   config.assets.compile = true
   # Settings specified here will take precedence over those in config/application.rb.
 
